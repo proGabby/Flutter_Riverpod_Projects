b547951d91a3e5de2c6829c20d19b20cab981304
proGabby <inimfonwillie@gmail.com>
2022-10-27 04:22:14 +0100

    working with stream provider

--- ex_1_app/README.md
+++ ex_1_app/README.md
@@ -1,1 +1,1 @@
-Using a state Notifier provider
+Using a stream provider to display a list of name where the name are display one after another ie stream
--- ex_1_app/lib/main.dart
+++ ex_1_app/lib/main.dart
@@ -10,25 +10,49 @@ void main() {
   );
 }
 
-//An extension method that add extra functionalities to the num class
-//It functionality is to enable us add an optional Int to an int
-//if the 1st operand is an int
-extension OptionalInfixAddition<T extends num> on T? {
-  T? operator +(T? other) {
-    final shadow = this;
-    if (shadow != null) {
-      return shadow + (other ?? 0) as T;
-    } else {
-      return null;
-    }
+const names = [
+  "Abigail",
+  "George",
+  "Bassey",
+  "John",
+  "Micheal",
+  "Eden",
+  "Lois",
+  "Silver"
+];
+
+class Reset extends StateNotifier<bool> {
+  Reset() : super(false);
+
+  void resetState() {
+    state = !state;
   }
 }
 
-// void testIt() {
-//   final int? int1 = 1;
-//   final int? int2 = null;
-//   final result = int1 + int2;
-// }
+final resetProvider = StateNotifierProvider<Reset, bool>((ref) => Reset());
+//produces a stream value every seconds
+final tickerProvider = StreamProvider((ref) {
+  final _isReset = ref.watch(resetProvider);
+  return _isReset
+      ? Stream.periodic(
+          Duration(seconds: 1),
+          (num) => num + 1,
+        )
+      : Stream.periodic(
+          Duration(seconds: 1),
+          (num) => num + 1,
+        );
+});
+
+//produces a iterable of names ranging from the index 0 to num provider by tickerprovider
+//if tickprovider produces a num that is not an index of names, it will throw an error
+final nameProvider = StreamProvider((ref) {
+  //read and listen to the tickerProvider value
+  final tickstream = ref.watch(tickerProvider.stream);
+  //.map transforms each element of this stream into a new stream event.
+  //produce range of value of name eg [ nameA, nameB] or [nameA, nameB, nameC, nameD] yields depending the result of tickerprovider
+  return tickstream.map((event) => names.getRange(0, event));
+});
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -37,96 +61,45 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Demo App',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark(),
       home: MainPage(),
     );
   }
 }
 
-final CurrentDate = Provider(
-  (ref) => DateTime.now(),
-  //here provider does not observe changes
-);
-
-class Counter extends StateNotifier<int?> {
-  Counter() : super(null);
-
-  void increment() {
-    //without the extionsion function OptionalInfixAddition, it will throw an error
-    state = (state == null) ? 1 : state + 1;
-  }
-
-  void reset() {
-    state = 1;
-  }
-
-  //state is the current state of this [StateNotifier].
-  //it always available in a stateNotifier
-  int? get value => state;
-}
-
-//
-//A global stateNotifierProvider
-//Note: the Counter class doesnt neccessary need to be in same file
-//as ref with providerScope will ensure it will always be available any where
-final counterProvider = StateNotifierProvider<Counter, int?>(
-  (ref) => Counter(),
-);
-
 class MainPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    //Note using ref.watch before the widget will make the build method run anytime there is a change of state
-    //which is performance optimistic... Hence, we use a consumer widget
-    // final counter = ref.watch(counterProvider);
+    //watching the nameProvider for values and update
+    final names = ref.watch(nameProvider);
+    final resetter = ref.read(resetProvider.notifier);
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text("Riverpod")),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(
-              height: 200,
-            ),
-            //Note the WidgetRef of the consumer is differrent from that of the build method
-            //this ensure only the consumer widget is build not the entire screen
-            Consumer(
-                builder: (BuildContext context, WidgetRef ref, Widget? child) {
-              final counterValue = ref.watch(counterProvider);
-              final valueString = counterValue == null
-                  ? 'Press the button'
-                  : counterValue.toString();
-              return Text(valueString);
-              //Note the child of the consumer is a widget we dont want to rebuild
-            }),
-
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextButton(
-                    onPressed: () {
-                      //notifier is the notifier of the stateNotifier class
-                      //it obtain the state from the stateNotifier class
-                      //Note: we cant access the counter class methods with counterProvider.notifier alone
-                      //hence we use a widgetRef to read or watch
-                      //here, we use read. read allow us to only read the current state and not the next changes
-                      ref.read(counterProvider.notifier).increment();
-                    },
-                    child: const Text("Increase")),
-                TextButton(
-                    onPressed: () {
-                      ref.read(counterProvider.notifier).reset();
-                    },
-                    child: const Text("reset")),
-              ],
-            )
-          ],
+        actions: [
+          TextButton(
+              onPressed: () {
+                resetter.resetState();
+              },
+              child: const Text("restart"))
+        ],
+        title: const Center(
+          child: Center(child: Text("Stream Provider")),
         ),
+      ),
+      body: names.when(
+        // since names is an async value, we can use the .when property
+        data: (names) {
+          return ListView.builder(
+              itemCount: names.length,
+              itemBuilder: (context, index) => ListTile(
+                    title: Text(
+                      names.elementAt(index),
+                    ),
+                  ));
+        },
+        error: (error, stackTrace) => const Center(child: Text("Done Listing")),
+        loading: () => const Center(child: CircularProgressIndicator()),
       ),
     );
   }
